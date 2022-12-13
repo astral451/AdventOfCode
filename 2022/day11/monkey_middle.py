87381cb94ec4f3d5d0871073c463285a5a7467f4
astral451 <develop fan detector>
2022-12-12 20:34:22 -0600

    Still no idea on day 11.2

--- 2022/day11/monkey_middle.py
+++ 2022/day11/monkey_middle.py
@@ -30,7 +30,7 @@ class Item:
     def __init__(self, value):
         self.value = value
 
-    def calculate_worry(self, arg1, operator, arg2, divided=0, mod=0):
+    def calculate_worry(self, arg1, operator, arg2):
         if arg1 == 'old':
             a_1 = self.value
         else:
@@ -50,13 +50,8 @@ class Item:
         elif operator == '/':
             value = a_1 / a_2
 
-        if divided != 0:
-            value = int(value / float(divided))
+        self.value = int(value)
 
-        if mod > 0:
-            value = value % mod
-
-        self.value = value
 
 class Monkey:
     def __init__(self, name, items, operations, test):
@@ -89,6 +84,8 @@ class Monkey:
 def process_monkeys(monkey_data):
     
     re_operation = re.compile(r'(old|\d+) ([\+\-\*\/]) (old|\d+)')
+    global MONKEYS
+    MONKEYS = dict()
 
     monkey_idx = 0
     for idx, line in enumerate(monkey_data):
@@ -137,16 +134,8 @@ def pass_item_to_monkey(item, monkey_idx2):
     MONKEYS[monkey_idx2].items.append(item)
 
 
-if __name__ == "__main__":
-
-    file_name = 'money_middle_data.txt'
-    file_name = 'temp_monkey_middle.txt'
-    file_path = pathlib.Path(__file__)
-    folder_path = pathlib.Path(file_path.parent, file_name)
-    monkey_data = load_data_file(folder_path.as_posix()).split('\n')
-
+def process_loop(input_value):
     process_monkeys(monkey_data)
-
     _ranges = range(1000,11000,1000)
     range_to_test = [0, 20] + list(_ranges)
     for idx in range(0, 10000):
@@ -156,7 +145,8 @@ if __name__ == "__main__":
             monkey.items = []
             for item in monkey_items:
                 # item.calculate_worry(*monkey.operations, divided=monkey.inspection_times) #, mod=int(m_idx)+1)
-                item.calculate_worry(*monkey.operations, divided=0, mod=monkey.inspection_times) #, mod=int(m_idx)+1)
+                item.calculate_worry(*monkey.operations) #, mod=int(m_idx)+1)
+                item.value = int(item.value - (item.value / 2))
                 pass_to_monkey_idx = monkey.test_value(item.value)
                 pass_item_to_monkey(item, pass_to_monkey_idx)
 
@@ -169,4 +159,23 @@ if __name__ == "__main__":
     inspections = sorted([(m.name,m.inspection_times) for m in MONKEYS.values()],reverse=True, key=lambda x:x[0])
     print(inspections)
     print(inspections[0][1] * inspections[1][1])
+
+
+
+if __name__ == "__main__":
+
+    file_name = 'money_middle_data.txt'
+    file_name = 'temp_monkey_middle.txt'
+    file_path = pathlib.Path(__file__)
+    folder_path = pathlib.Path(file_path.parent, file_name)
+    monkey_data = load_data_file(folder_path.as_posix()).split('\n')
+
+
+    for i in range(1,2):
+        print('Processing with value of {}'.format(i))
+        try:
+            process_loop(i)
+        except Exception as excp:
+            print('excp was :{0}'.format(excp))
+            continue
 
